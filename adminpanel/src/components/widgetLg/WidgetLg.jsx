0d3c615aaf940e2439951af0fb94fdf3fadbe62b
Khalfan1998 <khalfan.ikns@gmail.com>
2022-01-26 15:49:44 +0300

    WidgetLg
    latest transactions

--- adminpanel/src/components/widgetLg/WidgetLg.jsx
+++ adminpanel/src/components/widgetLg/WidgetLg.jsx
@@ -3,7 +3,7 @@ import "./widgetLg.css";
 export default function WidgetLg() {
 
     const Button = ({type}) => {
-        return <button className="widgetLgButton">{type}</button>
+        return <button className={"widgetLgButton "+ type}>{type}</button>
     }
   return <div className="widgetLg">
       <h3 className="widgetLgTitle">Latest Transactions</h3>
@@ -21,7 +21,43 @@ export default function WidgetLg() {
           </td>
           <td className="widgetLgDate">21st June 2021</td>
           <td className="widgetLgDate">BHD260.00</td>
-          <td className="widgetLgOrderStatus"></td>
+          <td className="widgetLgOrderStatus"><Button type="Approved"/></td>
+      </tr>
+      <tr className="widgetLgTr">
+          <td className="widgetLgUser">
+              <img src="https://i.postimg.cc/13MnLztm/mika-1.png" alt="User" className="widgetLgImg" />
+            <span className="widgetLgName">James Davis</span>
+          </td>
+          <td className="widgetLgDate">21st June 2021</td>
+          <td className="widgetLgDate">BHD260.00</td>
+          <td className="widgetLgOrderStatus"><Button type="Declined"/></td>
+      </tr>
+      <tr className="widgetLgTr">
+          <td className="widgetLgUser">
+              <img src="https://i.postimg.cc/13MnLztm/mika-1.png" alt="User" className="widgetLgImg" />
+            <span className="widgetLgName">James Davis</span>
+          </td>
+          <td className="widgetLgDate">21st June 2021</td>
+          <td className="widgetLgDate">BHD260.00</td>
+          <td className="widgetLgOrderStatus"><Button type="Pending"/></td>
+      </tr>
+      <tr className="widgetLgTr">
+          <td className="widgetLgUser">
+              <img src="https://i.postimg.cc/13MnLztm/mika-1.png" alt="User" className="widgetLgImg" />
+            <span className="widgetLgName">James Davis</span>
+          </td>
+          <td className="widgetLgDate">21st June 2021</td>
+          <td className="widgetLgDate">BHD260.00</td>
+          <td className="widgetLgOrderStatus"><Button type="Approved"/></td>
+      </tr>
+      <tr className="widgetLgTr">
+          <td className="widgetLgUser">
+              <img src="https://i.postimg.cc/13MnLztm/mika-1.png" alt="User" className="widgetLgImg" />
+            <span className="widgetLgName">James Davis</span>
+          </td>
+          <td className="widgetLgDate">21st June 2021</td>
+          <td className="widgetLgDate">BHD260.00</td>
+          <td className="widgetLgOrderStatus"><Button type="Pending"/></td>
       </tr>
       </table>
       </div>;
