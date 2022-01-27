95574e423f48f9497e2abf618fc72576df731504
Khalfan1998 <khalfan.ikns@gmail.com>
2022-01-27 10:41:45 +0300

    User list table
    adding user list page

--- adminpanel/src/components/widgetLg/WidgetLg.jsx
+++ adminpanel/src/components/widgetLg/WidgetLg.jsx
@@ -26,37 +26,37 @@ export default function WidgetLg() {
       <tr className="widgetLgTr">
           <td className="widgetLgUser">
               <img src="https://i.postimg.cc/13MnLztm/mika-1.png" alt="User" className="widgetLgImg" />
-            <span className="widgetLgName">James Davis</span>
+            <span className="widgetLgName">Alex Philip</span>
           </td>
           <td className="widgetLgDate">21st June 2021</td>
-          <td className="widgetLgDate">BHD260.00</td>
+          <td className="widgetLgDate">BHD460.00</td>
           <td className="widgetLgOrderStatus"><Button type="Declined"/></td>
       </tr>
       <tr className="widgetLgTr">
           <td className="widgetLgUser">
               <img src="https://i.postimg.cc/13MnLztm/mika-1.png" alt="User" className="widgetLgImg" />
-            <span className="widgetLgName">James Davis</span>
+            <span className="widgetLgName">George Robinson</span>
           </td>
           <td className="widgetLgDate">21st June 2021</td>
-          <td className="widgetLgDate">BHD260.00</td>
+          <td className="widgetLgDate">BHD160.00</td>
           <td className="widgetLgOrderStatus"><Button type="Pending"/></td>
       </tr>
       <tr className="widgetLgTr">
           <td className="widgetLgUser">
               <img src="https://i.postimg.cc/13MnLztm/mika-1.png" alt="User" className="widgetLgImg" />
-            <span className="widgetLgName">James Davis</span>
+            <span className="widgetLgName">Ali Ahmad</span>
           </td>
           <td className="widgetLgDate">21st June 2021</td>
-          <td className="widgetLgDate">BHD260.00</td>
+          <td className="widgetLgDate">BHD60.00</td>
           <td className="widgetLgOrderStatus"><Button type="Approved"/></td>
       </tr>
       <tr className="widgetLgTr">
           <td className="widgetLgUser">
               <img src="https://i.postimg.cc/13MnLztm/mika-1.png" alt="User" className="widgetLgImg" />
-            <span className="widgetLgName">James Davis</span>
+            <span className="widgetLgName">Yousif Khalifa</span>
           </td>
           <td className="widgetLgDate">21st June 2021</td>
-          <td className="widgetLgDate">BHD260.00</td>
+          <td className="widgetLgDate">BHD45.00</td>
           <td className="widgetLgOrderStatus"><Button type="Pending"/></td>
       </tr>
       </table>
--- adminpanel/src/components/widgetSm/WidgetSm.jsx
+++ adminpanel/src/components/widgetSm/WidgetSm.jsx
@@ -30,7 +30,7 @@ export default function WidgetSm() {
           <li className="widgetSmListItem">
               <img src="https://i.postimg.cc/XqWNh7Nn/charles-deluvio-K4m-SJ7kc0-As-unsplash.jpg" alt="memberImg" className="widgetSmImg" />
               <div className="widgetSmUser">
-                  <span className="widgetSmUsername">Jack Johnson</span>
+                  <span className="widgetSmUsername">Alex Philip</span>
                   <span className="widgetSmUserTitle">Software Developer</span>
               </div>
               <button className="widgetSmButton">
@@ -41,7 +41,7 @@ export default function WidgetSm() {
           <li className="widgetSmListItem">
               <img src="https://i.postimg.cc/XqWNh7Nn/charles-deluvio-K4m-SJ7kc0-As-unsplash.jpg" alt="memberImg" className="widgetSmImg" />
               <div className="widgetSmUser">
-                  <span className="widgetSmUsername">Jack Johnson</span>
+                  <span className="widgetSmUsername">George Robinson</span>
                   <span className="widgetSmUserTitle">Software Developer</span>
               </div>
               <button className="widgetSmButton">
@@ -52,7 +52,7 @@ export default function WidgetSm() {
           <li className="widgetSmListItem">
               <img src="https://i.postimg.cc/XqWNh7Nn/charles-deluvio-K4m-SJ7kc0-As-unsplash.jpg" alt="memberImg" className="widgetSmImg" />
               <div className="widgetSmUser">
-                  <span className="widgetSmUsername">Jack Johnson</span>
+                  <span className="widgetSmUsername">Ali Ahmad</span>
                   <span className="widgetSmUserTitle">Software Developer</span>
               </div>
               <button className="widgetSmButton">
