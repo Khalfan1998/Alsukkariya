a4f01d3f60bd5c400d3fa25be7b2c746ee49ea2e
Khalfan1998 <khalfan.ikns@gmail.com>
2022-01-30 10:39:04 +0300

    User List
    delete row button

--- adminpanel/src/pages/userList/UserList.jsx
+++ adminpanel/src/pages/userList/UserList.jsx
@@ -3,8 +3,15 @@ import { DataGrid } from '@material-ui/data-grid';
 import { AccountCircle, DeleteOutline } from "@material-ui/icons";
 import { userRows } from "../../dummyData";
 import {Link} from "react-router-dom"
+import { useState } from "react";
 
 export default function UserList() {
+  const [data,setData] = useState(userRows)
+
+  const handleDelete = (id)=>{
+    setData(data.filter(item=>item.id !== id))
+  };
+
     const columns = [
         { field: 'id', headerName: 'ID', width: 140 },
         { field: 'user', headerName: 'Username', width: 180, renderCell: (params)=>{
@@ -36,7 +43,7 @@ export default function UserList() {
               <Link to={"/user/"+params.row.id}>
               <button className="userListEdit">Edit</button>
               </Link>
-              <DeleteOutline className="userListDelete"/>
+              <DeleteOutline className="userListDelete" onClick={()=> handleDelete(params.row.id)}/>
               </>
             )
           }
@@ -47,7 +54,7 @@ export default function UserList() {
       
   return <div className="userList"> 
 <DataGrid
-        rows={userRows} disableSelectionOnClick
+        rows={data} disableSelectionOnClick
         columns={columns}
         pageSize={10}
         rowsPerPageOptions={[5]}
