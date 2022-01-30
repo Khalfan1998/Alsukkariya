908c1f63f8b2e0abbee66314f5adf354c336020f
Khalfan1998 <khalfan.ikns@gmail.com>
2022-01-30 11:28:48 +0300

    User
    adding user page admin panel

---
+++ adminpanel/src/pages/user/User.jsx
@@ -0,0 +1,96 @@
+import "./userNew.css"
+import { AccountCircle, CalendarToday, LocationSearching, MailOutline, PermIdentity, PhoneAndroid, Publish } from "@material-ui/icons";
+
+export default function User() {
+  return <div className="userOne">
+       <div className="userTitleContainer">
+          <h1 className="userTitle">Edit User</h1>
+          <button className="userAddButton">Create</button>
+      </div>
+      <div className="userContainer">
+          <div className="userShow">
+            <div className="userShowTop">
+            <AccountCircle className="userShowImg"/>
+            <div className="userShowTopTitle">
+                <span className="userShowUsername">Jack Davis</span>
+                <span className="userShowTitle">Software Developer</span>
+            </div>
+            </div>
+            <div className="userShowBottom">
+                <span className="userShowTitle">Account Details</span>
+                <div className="userShowInfo">
+                <PermIdentity className="userShowIcon"/>
+                <span className="userShowInfoTitle">jackdavis44</span>
+                </div>
+                <div className="userShowInfo">
+                <CalendarToday className="userShowIcon"/>
+                <span className="userShowInfoTitle">24.06.1990</span>
+                </div>
+                <span className="userShowTitle">Contact Details</span>
+                <div className="userShowInfo">
+                <PhoneAndroid className="userShowIcon"/>                
+                <span className="userShowInfoTitle">+9733939393</span>
+                </div>
+                <div className="userShowInfo">
+                <MailOutline className="userShowIcon"/>
+                <span className="userShowInfoTitle">jackdavis44@gmail.com</span>
+                </div>
+                <div className="userShowInfo">
+                <LocationSearching className="userShowIcon"/>
+                <span className="userShowInfoTitle">House44 | Area 110 | Manama</span>
+                </div>
+            </div>
+          </div>
+          <div className="userUpdate">
+            <span className="userUpdateTitle">Edit</span>
+            <form className="userUpdateForm">
+                <div className="userUpdateLeft">
+                    <div className="userUpdateItem">
+                        <label>Username</label>
+                        <input 
+                        type="text" 
+                        placeholder="jackdavis44" 
+                        className="userUpdateInput" />
+                    </div>
+                    <div className="userUpdateItem">
+                        <label>Full Name</label>
+                        <input 
+                        type="text" 
+                        placeholder="Jack Davis" 
+                        className="userUpdateInput" />
+                    </div>
+                    <div className="userUpdateItem">
+                        <label>Email</label>
+                        <input 
+                        type="text" 
+                        placeholder="jackdavis44@gmail.com" 
+                        className="userUpdateInput" />
+                    </div>
+                    <div className="userUpdateItem">
+                        <label>Phone</label>
+                        <input 
+                        type="text" 
+                        placeholder="+9733939393" 
+                        className="userUpdateInput" />
+                    </div>
+                    <div className="userUpdateItem">
+                        <label>Address</label>
+                        <input 
+                        type="text" 
+                        placeholder="House44 | Area 110 | Manama" 
+                        className="userUpdateInput" />
+                    </div>
+                </div>
+                <div className="userUpdateRight">
+                    <div className="userUpdateUpload">
+                        <AccountCircle className="userUpdateImg" />
+                        <label htmlFor="file"><Publish className="userUpdateIcon"/></label>
+                        <input type="file" id="file" style={{display:"none"}} />
+                    </div>
+                    <button className="userUpdateButton">Update</button>
+                </div>
+            </form>
+          </div>
+      </div>
+  </div>;
+}
