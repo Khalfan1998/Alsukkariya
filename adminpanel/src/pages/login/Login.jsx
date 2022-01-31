82560cd8d879f57d405c5d25fca3ac0569dde42c
Khalfan1998 <khalfan.ikns@gmail.com>
2022-01-31 11:19:44 +0300

    admin login

--- adminpanel/src/pages/login/Login.jsx
+++ adminpanel/src/pages/login/Login.jsx
@@ -1,19 +1,27 @@
 import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { login } from '../../redux/apiCalls';
 
 
 const Login = () => {
 
 const [username,setUsername] = useState("")
 const [password,setPassword] = useState("")
+const dispatch = useDispatch()
 
 const handleClick = (e) => {
     e.preventDefault()
+    login(dispatch,{username,password})
 }
 
-  return <div>
-    <input type="text" placeholder="username" onChange={e=>setUsername(e.target.value)}/>
-    <input type="password" placeholder="password" onChange={e=>setPassword(e.target.value)}/>
-    <button onClick={handleClick}>Login</button>
+  return <div style={{display: "flex",
+  flexDirection: "column",
+   alignItems: "center", 
+   justifyContent: "center", 
+   height:"100vh"}}>
+    <input style={{padding: 10 , marginBottom:20}} type="text" placeholder="username" onChange={e=>setUsername(e.target.value)}/>
+    <input style={{padding: 10 , marginBottom:20}} type="password" placeholder="password" onChange={e=>setPassword(e.target.value)}/>
+    <button style={{padding:10, width:100}} onClick={handleClick}>Login</button>
   </div>;
 };
 
