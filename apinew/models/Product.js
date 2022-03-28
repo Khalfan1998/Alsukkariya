7bb112a071a417f1d0633e289852a486b3e39de8
Khalfan1998 <khalfan.ikns@gmail.com>
2022-03-28 10:59:35 +0300

    my newest commit

--- apinew/models/Product.js
+++ apinew/models/Product.js
@@ -2,7 +2,6 @@ const mongoose = require("mongoose");
 
 const ProductSchema = new mongoose.Schema(
   {
-    _id: mongoose.ObjectId,
     title: { type: String, required: true, unique: true },
     desc: { type: String, required: true },
     img: { type: String, required: true },
