8adbaec274e38398416d421533e7c046f3dbe8ed
Khalfan1998 <khalfan.ikns@gmail.com>
2022-03-29 13:51:17 +0300

    my 34th commit

--- apinew/index.js
+++ apinew/index.js
@@ -26,11 +26,11 @@ mongoose
 
 app.use(cors());
 app.use(express.json());
+app.use("/api/products", productRoute);
+app.use("/api/orders", orderRoute);
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
-app.use("/api/products", productRoute);
 app.use("/api/carts", cartRoute);
-app.use("/api/orders", orderRoute);
 app.use("/api/checkout", stripeRoute);
 
 app.use(express.static(path.join(__dirname, "/clientnew/build")));
